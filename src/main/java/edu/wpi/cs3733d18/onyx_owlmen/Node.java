324ac2eb6a196bca2318d6cfc730eb36f326e953
jcharante <git@jcharante.com>
2019-03-25 19:42:34 -0400

    add node classes

---
+++ src/main/java/edu/wpi/cs3733d18/onyx_owlmen/Node.java
@@ -0,0 +1,12 @@
+package edu.wpi.cs3733d18.onyx_owlmen;
+
+public class Node {
+    String nodeID;
+    int xcoord;
+    int ycoord;
+    int floor;
+    String building;
+    NodeType nodeType;
+    String longName;
+    String shortName;
+}
---
+++ src/main/java/edu/wpi/cs3733d18/onyx_owlmen/NodeType.java
@@ -0,0 +1,12 @@
+package edu.wpi.cs3733d18.onyx_owlmen;
+
+public enum NodeType {
+    CONF,
+    HALL,
+    DEPT,
+    INFO,
+    LABS,
+    REST,
+    SERV,
+    STAI
+}
