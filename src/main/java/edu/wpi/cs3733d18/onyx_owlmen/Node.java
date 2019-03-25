3f8c959b422ab0d63c69155fbc3aadc146de5cf5
jcharante <git@jcharante.com>
2019-03-25 19:48:57 -0400

    add stubs

--- src/main/java/edu/wpi/cs3733d18/onyx_owlmen/Node.java
+++ src/main/java/edu/wpi/cs3733d18/onyx_owlmen/Node.java
@@ -9,4 +9,15 @@ public class Node {
     NodeType nodeType;
     String longName;
     String shortName;
+
+    public Node(String nodeID, int xcoord, int ycoord, int floor, String building, NodeType nodeType, String longName, String shortName) {
+        this.nodeID = nodeID;
+        this.xcoord = xcoord;
+        this.ycoord = ycoord;
+        this.floor = floor;
+        this.building = building;
+        this.nodeType = nodeType;
+        this.longName = longName;
+        this.shortName = shortName;
+    }
 }
