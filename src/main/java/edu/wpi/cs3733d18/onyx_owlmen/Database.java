d39b99c8111f8912a40e2bb9293dd828d127d4bc
jcharante <git@jcharante.com>
2019-03-25 20:14:57 -0400

    add formatting to pass tests

--- src/main/java/edu/wpi/cs3733d18/onyx_owlmen/Database.java
+++ src/main/java/edu/wpi/cs3733d18/onyx_owlmen/Database.java
@@ -1,43 +1,44 @@
 package edu.wpi.cs3733d18.onyx_owlmen;
 
+import java.util.Collection;
 import java.util.LinkedList;
 
 public class Database {
-    public Database() {
+  public Database() {
 
-    }
+  }
 
 
-    LinkedList<Node> getNodes() {
-        LinkedList<Node> ll = new LinkedList<>();
-        ll.add(new Node(
-            "BCONF00102",
-                2150,
-                1025,
-                2,
-                "45 Francis",
-                NodeType.CONF,
-                "Duncan Reid Conference Room",
-                "Conf B0102"
-        ));
-        ll.add(new Node(
-            "BHALL03802",
-                2279,
-                786,
-                2,
-                "45 Francis",
-                NodeType.HALL,
-                "Hallway Intersection 38 Level 2",
-                "Hallway B3802"
-        ));
-        return ll;
-    }
+  Collection<Node> getNodes() {
+    LinkedList<Node> ll = new LinkedList<>();
+    ll.add(new Node(
+        "BCONF00102",
+        2150,
+        1025,
+        2,
+        "45 Francis",
+        NodeType.CONF,
+        "Duncan Reid Conference Room",
+        "Conf B0102"
+    ));
+    ll.add(new Node(
+        "BHALL03802",
+        2279,
+        786,
+        2,
+        "45 Francis",
+        NodeType.HALL,
+        "Hallway Intersection 38 Level 2",
+        "Hallway B3802"
+    ));
+    return ll;
+  }
 
-    void updateNode(String nodeId, Node newNode) {
-        //
-    }
+  void updateNode(String nodeId, Node newNode) {
+    //
+  }
 
-    void deleteNode(String nodeId) {
-        //
-    }
+  void deleteNode(String nodeId) {
+    //
+  }
 }
--- src/main/java/edu/wpi/cs3733d18/onyx_owlmen/Node.java
+++ src/main/java/edu/wpi/cs3733d18/onyx_owlmen/Node.java
@@ -1,23 +1,41 @@
 package edu.wpi.cs3733d18.onyx_owlmen;
 
 public class Node {
-    String nodeID;
-    int xcoord;
-    int ycoord;
-    int floor;
-    String building;
-    NodeType nodeType;
-    String longName;
-    String shortName;
+  String nodeID;
+  int xcoord;
+  int ycoord;
+  int floor;
+  String building;
+  NodeType nodeType;
+  String longName;
+  String shortName;
 
-    public Node(String nodeID, int xcoord, int ycoord, int floor, String building, NodeType nodeType, String longName, String shortName) {
-        this.nodeID = nodeID;
-        this.xcoord = xcoord;
-        this.ycoord = ycoord;
-        this.floor = floor;
-        this.building = building;
-        this.nodeType = nodeType;
-        this.longName = longName;
-        this.shortName = shortName;
-    }
+  /**
+   * Constructor for the node class.
+   * @param nodeID The unique identifier for the node
+   * @param xcoord The X coordinate for the node
+   * @param ycoord The Y coordinate for the node
+   * @param floor The floor of the node
+   * @param building The building address of the Node
+   * @param nodeType The type of node
+   * @param longName The name of the node
+   * @param shortName A shorter name of the node
+   */
+  public Node(String nodeID,
+              int xcoord,
+              int ycoord,
+              int floor,
+              String building,
+              NodeType nodeType,
+              String longName,
+              String shortName) {
+    this.nodeID = nodeID;
+    this.xcoord = xcoord;
+    this.ycoord = ycoord;
+    this.floor = floor;
+    this.building = building;
+    this.nodeType = nodeType;
+    this.longName = longName;
+    this.shortName = shortName;
+  }
 }
