3fd9c449fcd58a7b5a6e259e7020aa151912bf19
jcharante <git@jcharante.com>
2019-03-26 20:44:12 -0400

    refactor the UI components out of Node into UINode

--- src/main/java/edu/wpi/cs3733d18/onyx_owlmen/database_prototype/Node.java
+++ src/main/java/edu/wpi/cs3733d18/onyx_owlmen/database_prototype/Node.java
@@ -2,13 +2,6 @@ package edu.wpi.cs3733d18.onyx_owlmen.database_prototype;
 
 import java.util.Locale;
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import javafx.scene.control.Button;
-
 public class Node {
   String nodeID;
   int xcoord;
@@ -18,9 +11,6 @@ public class Node {
   NodeType nodeType;
   String longName;
   String shortName;
-
-  Button editButton; // ????
-  Button deleteButton;
 
   static Node parseNode(String str) {
     String[] fields = str.split(",");
@@ -97,53 +87,9 @@ public class Node {
     this.nodeType = nodeType;
     this.longName = longName;
     this.shortName = shortName;
-    this.editButton = new Button("Edit");
-    this.deleteButton = new Button("Delete");
-
   }
 
-
-  /**
-   * These methods are called automatically by PropertyValueFactory
-   * when populating the table with data.
-   */
-  public StringProperty nodeIDProperty() {
-    return new SimpleStringProperty(nodeID);
-  }
-
-  public IntegerProperty xcoordProperty() {
-    return new SimpleIntegerProperty(xcoord);
-  }
-
-  public IntegerProperty ycoordProperty() {
-    return new SimpleIntegerProperty(ycoord);
-  }
-
-  public IntegerProperty floorProperty() {
-    return new SimpleIntegerProperty(floor);
-  }
-
-  public StringProperty buildingProperty() {
-    return new SimpleStringProperty(building);
-  }
-
-  public SimpleObjectProperty<NodeType> nodeTypeProperty() {
-    return new SimpleObjectProperty<>(nodeType);
-  }
-
-  public StringProperty longNameProperty() {
-    return new SimpleStringProperty(longName);
-  }
-
-  public StringProperty shortNameProperty() {
-    return new SimpleStringProperty(shortName);
-  }
-
-  public SimpleObjectProperty<Button> editButtonProperty() {
-    return new SimpleObjectProperty<>(editButton);
-  }
-
-  public SimpleObjectProperty<Button> deleteButtonProperty() {
-    return new SimpleObjectProperty<>(deleteButton);
+  public UINode asUINode() {
+    return new UINode(this);
   }
 }
