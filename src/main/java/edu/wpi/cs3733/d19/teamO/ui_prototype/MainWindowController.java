ffe01383843d84146f6f3eaee459bac5dfe59c08
Ken Morton <40545486+kjmj@users.noreply.github.com>
2019-03-29 19:08:15 -0400

    fix path for fxml files (#89)

--- src/main/java/edu/wpi/cs3733/d19/teamO/ui_prototype/MainWindowController.java
+++ src/main/java/edu/wpi/cs3733/d19/teamO/ui_prototype/MainWindowController.java
@@ -1,5 +1,47 @@
 package edu.wpi.cs3733.d19.teamO.ui_prototype;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MainWindowController {
 
+  @FXML
+  private Button navigationButton;
+
+  @FXML
+  private Button mapToolButton;
+
+  @FXML
+  private Button sanitationRequestButton;
+
+  @FXML
+  private Button securityRequestButton;
+
+  @FXML
+  private Button schedulingButton;
+
+  //  @FXML
+  //  private void navigationButtonAction() {
+  //
+  //  }
+  //
+  //  @FXML
+  //  private void mapToolButtonAction() {
+  //
+  //  }
+  //
+  //  @FXML
+  //  private void sanitationRequestButtonAction() {
+  //
+  //  }
+  //
+  //  @FXML
+  //  private void securityRequestButtonAction() {
+  //
+  //  }
+  //
+  //  @FXML
+  //  private void schedulingButtonButtonAction() {
+  //
+  //  }
 }
