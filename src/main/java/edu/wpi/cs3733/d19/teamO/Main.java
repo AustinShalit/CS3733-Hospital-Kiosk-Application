89b27cf93a2114580e5c46e751414ef098cb0525
Austin Shalit <austinshalit@gmail.com>
2019-03-29 14:06:44 -0400

    Add CSV Writer & Reader

--- src/main/java/edu/wpi/cs3733/d19/teamO/Main.java
+++ src/main/java/edu/wpi/cs3733/d19/teamO/Main.java
@@ -8,7 +8,7 @@ public final class Main {
     throw new UnsupportedOperationException("This is a utility class!");
   }
 
-  public static void main(String... args) {
+  public static void main(final String... args) {
     Application.launch(Project.class, args);
   }
 }
