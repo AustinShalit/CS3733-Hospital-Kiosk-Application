9f4a5340d76729b6c1043cb6900ebddd688bd319
Lynn <jsong@wpi.edu>
2019-03-30 15:51:44 -0400

    Travis should pass

--- src/main/java/edu/wpi/cs3733/d19/teamO/entity/ServiceRequest.java
+++ src/main/java/edu/wpi/cs3733/d19/teamO/entity/ServiceRequest.java
@@ -4,40 +4,41 @@ import java.time.LocalDateTime;
 
 class ServiceRequest {
 
-    private final LocalDateTime timeRequested;
-    private final LocalDateTime timeCompleted;
-    private final String whoCompleted;
-    private final String description;
-    private final Node node;
+  private final LocalDateTime timeRequested;
+  private final LocalDateTime timeCompleted;
+  private final String whoCompleted;
+  private final String description;
+  private final Node node;
 
-    // constructor
-    ServiceRequest(LocalDateTime timeRequested, LocalDateTime timeCompleted, String whoCompleted, String description, Node node){
-        this.timeRequested = timeRequested;
-        this.timeCompleted = timeCompleted;
-        this.whoCompleted = whoCompleted;
-        this.description = description;
-        this.node = node;
-    }
+  // constructor
+  ServiceRequest(LocalDateTime timeRequested, LocalDateTime timeCompleted, String whoCompleted,
+                 String description, Node node) {
+    this.timeRequested = timeRequested;
+    this.timeCompleted = timeCompleted;
+    this.whoCompleted = whoCompleted;
+    this.description = description;
+    this.node = node;
+  }
 
-    // getters and setters
-    public LocalDateTime getTimeRequested() {
-        return timeRequested;
-    }
+  // getters and setters
+  public LocalDateTime getTimeRequested() {
+    return timeRequested;
+  }
 
-    public LocalDateTime getTimeCompleted() {
-        return timeCompleted;
-    }
+  public LocalDateTime getTimeCompleted() {
+    return timeCompleted;
+  }
 
-    public String getWhoCompleted() {
-        return whoCompleted;
-    }
+  public String getWhoCompleted() {
+    return whoCompleted;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    // functions
-    public Node getLocationNode(){
-            return node;
-    }
+  // functions
+  public Node getLocationNode() {
+    return node;
+  }
 }
