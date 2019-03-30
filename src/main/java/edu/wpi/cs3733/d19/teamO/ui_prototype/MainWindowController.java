10af5d502f32bedde83b85dc7b3e2002854ab04b
Ken Morton Jr <kmorton15@gmail.com>
2019-03-29 20:10:38 -0400

    functionality to switch scenes

--- src/main/java/edu/wpi/cs3733/d19/teamO/ui_prototype/MainWindowController.java
+++ src/main/java/edu/wpi/cs3733/d19/teamO/ui_prototype/MainWindowController.java
@@ -1,7 +1,14 @@
 package edu.wpi.cs3733.d19.teamO.ui_prototype;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainWindowController {
 
@@ -20,28 +27,35 @@ public class MainWindowController {
   @FXML
   private Button schedulingButton;
 
-  //  @FXML
-  //  private void navigationButtonAction() {
-  //
-  //  }
-  //
-  //  @FXML
-  //  private void mapToolButtonAction() {
-  //
-  //  }
-  //
-  //  @FXML
-  //  private void sanitationRequestButtonAction() {
-  //
-  //  }
-  //
-  //  @FXML
-  //  private void securityRequestButtonAction() {
-  //
-  //  }
-  //
-  //  @FXML
-  //  private void schedulingButtonButtonAction() {
-  //
-  //  }
+    @FXML
+    private void navigationButtonAction() throws IOException {
+
+    }
+
+    @FXML
+    private void mapToolButtonAction() {
+
+    }
+
+    @FXML
+    private void sanitationRequestButtonAction(ActionEvent event) throws IOException {
+      if(event.getSource() == sanitationRequestButton){
+        Stage stage = (Stage) sanitationRequestButton.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("SanitationWindow.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+      }
+    }
+
+    @FXML
+    private void securityRequestButtonAction() {
+
+    }
+
+    @FXML
+    private void schedulingButtonButtonAction() {
+
+    }
 }
