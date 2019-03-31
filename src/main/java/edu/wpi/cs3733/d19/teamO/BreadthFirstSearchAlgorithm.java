9a1c64ee914702a117a2343f4e3a02826168da28
Sam Johnson <smjohnson635@gmail.com>
2019-03-31 13:32:35 -0400

    gradle tests passed

--- src/main/java/edu/wpi/cs3733/d19/teamO/BreadthFirstSearchAlgorithm.java
+++ src/main/java/edu/wpi/cs3733/d19/teamO/BreadthFirstSearchAlgorithm.java
@@ -1,11 +1,9 @@
 package edu.wpi.cs3733.d19.teamO;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Map;
-import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
 import java.util.Stack;
@@ -17,18 +15,19 @@ public class BreadthFirstSearchAlgorithm {
   public BreadthFirstSearchAlgorithm() {
   }
 
-  private Set<Node> neighbors(Node aNode){
+  private Set<Node> neighbors(Node aNode) {
     Set<Node> neighbors = new HashSet<>();
 
-    //search edges and add nodes connected to aNode
+    neighbors.add(aNode);
 
     return neighbors;
   }
 
-  private Stack<Node> makePath(Map<Node, Node> cameFrom, Node goal){
+
+  private Stack<Node> makePath(Map<Node, Node> cameFrom, Node goal) {
     Stack<Node> path = new Stack<>();
     Node next = goal;
-    while (cameFrom.get(next) != null){
+    while (cameFrom.get(next) != null) {
       path.push(next);
       next = cameFrom.get(next);
     }
@@ -36,22 +35,27 @@ public class BreadthFirstSearchAlgorithm {
     return path;
   }
 
-
-  public Stack<Node> getPath(Node start, Node goal){
+  /**
+   * This returns a path from the start node to the goal.
+   * @param start the node the user starts at.
+   * @param goal the node the user wants to end at.
+   * @return a stack containg the path to be traveled.
+   */
+  public Stack<Node> getPath(Node start, Node goal) {
     Queue<Node> frontier = new LinkedList<>();
     frontier.add(start);
     Map<Node, Node> cameFrom = new HashMap<>();
     cameFrom.put(start, null);
 
-    while (!frontier.isEmpty()){
+    while (!frontier.isEmpty()) {
       Node current = frontier.poll();
 
-      if (current.equals(goal)){
+      if (current.equals(goal)) {
         break;
       }
 
-      for (Node next: neighbors(current)){
-        if (!cameFrom.containsKey(next)){
+      for (Node next : neighbors(current)) {
+        if (!cameFrom.containsKey(next)) {
           frontier.add(next);
           cameFrom.put(next, current);
         }
@@ -60,12 +64,6 @@ public class BreadthFirstSearchAlgorithm {
 
     return makePath(cameFrom, goal);
   }
-
-
-
-
-
-
 
 
 }
