cf08fdaa0dfda89d85867b01f9fbb310ee29f96d
Ken Morton Jr <kmorton15@gmail.com>
2019-04-02 17:32:32 -0400

    sanitation now reads nodes from the database

--- src/main/java/edu/wpi/cs3733/d19/teamO/controller/SanitationWindowController.java
+++ src/main/java/edu/wpi/cs3733/d19/teamO/controller/SanitationWindowController.java
@@ -1,13 +1,10 @@
 package edu.wpi.cs3733.d19.teamO.controller;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,13 +13,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 
 import edu.wpi.cs3733.d19.teamO.controller.exception.InvalidUserInputException;
 import edu.wpi.cs3733.d19.teamO.entity.Node;
 import edu.wpi.cs3733.d19.teamO.entity.SanitationRequest;
-import edu.wpi.cs3733.d19.teamO.entity.csv.NodeCsvReaderWriter;
 import edu.wpi.cs3733.d19.teamO.entity.database.Database;
 
 public class SanitationWindowController extends Controller {
@@ -43,18 +37,12 @@ public class SanitationWindowController extends Controller {
   private Database db;
 
   @FXML
-  void initialize() throws SQLException, IOException {
+  void initialize() throws SQLException {
     db = new Database();
-    //    Set<Node> nodes = db.getAllNodes();
+    Set<Node> nodes = db.getAllNodes();
 
-    // read from file for now, until database is populated
-    FileChooser chooser = new FileChooser();
-    File file = chooser.showOpenDialog(new Stage());
-    NodeCsvReaderWriter ncrw = new NodeCsvReaderWriter();
-    List<Node> nodes = ncrw.readNodes(Files.newBufferedReader(file.toPath()));
     for (Node n : nodes) {
       locationComboBox.getItems().add(n.getNodeId());
-      db.insertNode(n);
     }
 
     categoryComboBox.getItems().setAll(SanitationRequest.SanitationRequestType.values());
