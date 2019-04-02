6e1b495bd6aa496ce0daeb6ed65d189d074a54ec
Ken Morton Jr <kmorton15@gmail.com>
2019-04-02 01:11:18 -0400

    merged master in, code now uses most recent csv file reader

--- src/main/java/edu/wpi/cs3733/d19/teamO/controller/Controller.java
+++ src/main/java/edu/wpi/cs3733/d19/teamO/controller/Controller.java
@@ -1,20 +1,13 @@
 package edu.wpi.cs3733.d19.teamO.controller;
 
-import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.List;
 
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.Window;
-
-import edu.wpi.cs3733.d19.teamO.entity.Node;
-import edu.wpi.cs3733.d19.teamO.entity.csv.NodeCsvReaderWriter;
 
 class Controller {
 
@@ -38,18 +31,5 @@ class Controller {
     Scene scene = new Scene(root);
     stage.setScene(scene);
     stage.show();
-  }
-
-  /**
-   * Get a list of nodes from a csv file. Used in testing.
-   */
-  @FXML
-  List<Node> getNodesFromFile() throws IOException {
-    FileChooser chooser = new FileChooser();
-    NodeCsvReaderWriter ncrw = new NodeCsvReaderWriter();
-    chooser.setTitle("Choose CSV File");
-    File file = chooser.showOpenDialog(new Stage());
-
-    return ncrw.readNodes(file.toPath());
   }
 }
--- src/main/java/edu/wpi/cs3733/d19/teamO/controller/SanitationWindowController.java
+++ src/main/java/edu/wpi/cs3733/d19/teamO/controller/SanitationWindowController.java
@@ -1,6 +1,8 @@
 package edu.wpi.cs3733.d19.teamO.controller;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,9 +13,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 import edu.wpi.cs3733.d19.teamO.entity.Node;
 import edu.wpi.cs3733.d19.teamO.entity.SanitationRequest;
+import edu.wpi.cs3733.d19.teamO.entity.csv.NodeCsvReaderWriter;
 import edu.wpi.cs3733.d19.teamO.entity.database.Database;
 
 public class SanitationWindowController extends Controller {
@@ -39,7 +44,10 @@ public class SanitationWindowController extends Controller {
     //    Set<Node> nodes = db.getAllNodes();
 
     // read from file for now, until database is populated
-    List<Node> nodes = getNodesFromFile();
+    FileChooser chooser = new FileChooser();
+    File file = chooser.showOpenDialog(new Stage());
+    NodeCsvReaderWriter ncrw = new NodeCsvReaderWriter();
+    List<Node> nodes = ncrw.readNodes(Files.newBufferedReader(file.toPath()));
     for (Node n : nodes) {
       locationComboBox.getItems().add(n.getNodeId());
       db.insertNode(n);
