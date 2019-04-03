f6c4da40423a61b3872130b5929057b2c5a056a2
Alexandria Nogueras <aenogueras@wpi.edu>
2019-04-02 23:10:50 -0400

    security window controllers

--- src/main/java/edu/wpi/cs3733/d19/teamO/controller/SecurityWindowController.java
+++ src/main/java/edu/wpi/cs3733/d19/teamO/controller/SecurityWindowController.java
@@ -1,11 +1,19 @@
 package edu.wpi.cs3733.d19.teamO.controller;
 
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.Random;
+
 import com.jfoenix.controls.JFXComboBox;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
+
+import edu.wpi.cs3733.d19.teamO.entity.Node;
+import edu.wpi.cs3733.d19.teamO.entity.SecurityRequest;
+import edu.wpi.cs3733.d19.teamO.entity.database.Database;
 
 public class SecurityWindowController extends Controller {
 
@@ -17,6 +25,9 @@ public class SecurityWindowController extends Controller {
 
   @FXML
   private Button getnodesbutton;
+
+  @FXML
+  private Button viewSecurityRequestButton;
 
   @FXML
   private Text securityTitle;
@@ -44,6 +55,28 @@ public class SecurityWindowController extends Controller {
   void onBackButtonAction(ActionEvent event) {
     if (event.getSource() == backButton) {
       switchScenes("MainWindow.fxml", backButton.getScene().getWindow());
+    }
+  }
+
+  @FXML
+  void onViewSecurityAction(ActionEvent event) {
+    if (event.getSource() == viewSecurityRequestButton) {
+      switchScenes("SecurityEmployeeOverviewWindow.fxml",
+          viewSecurityRequestButton.getScene().getWindow());
+    }
+  }
+
+  @FXML
+  void onAlertAction(ActionEvent event) throws SQLException {
+    if (event.getSource() == alertbutton) {
+      Database db = new Database();
+      Random r = new Random();
+      int rand = r.nextInt();
+      Node node1 = new Node(""+r, 1, 1, 2, "fuller",
+          Node.NodeType.DEPT, "fuller commons","fc");
+      db.insertNode(node1);
+      db.insertSecurityRequest(new SecurityRequest(rand, LocalDateTime.now(), LocalDateTime.now(),
+          "A", "idk", node1));
     }
   }
 
