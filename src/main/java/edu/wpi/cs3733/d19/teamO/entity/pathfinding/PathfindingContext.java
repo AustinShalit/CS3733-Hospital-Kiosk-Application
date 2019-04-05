4e55e6386b2a81f1dfaf64cc06beb188d54260e6
Austin Shalit <austinshalit@gmail.com>
2019-04-05 14:07:51 -0400

    Use strategy pattern for pathfinding

---
+++ src/main/java/edu/wpi/cs3733/d19/teamO/entity/pathfinding/PathfindingContext.java
@@ -0,0 +1,22 @@
+package edu.wpi.cs3733.d19.teamO.entity.pathfinding;
+
+import java.util.List;
+
+import com.google.common.graph.Graph;
+
+public class PathfindingContext<T> {
+
+  private GraphSearchAlgorithm<T> strategy;
+
+  private PathfindingContext(GraphSearchAlgorithm<T> strategy) {
+    this.strategy = strategy;
+  }
+
+  public PathfindingContext() {
+    this(new BreadthFirstSearchAlgorithm<>());
+  }
+
+  public List<T> getPath(Graph<T> graph, T start, T goal) {
+    return strategy.getPath(graph, start, goal);
+  }
+}
