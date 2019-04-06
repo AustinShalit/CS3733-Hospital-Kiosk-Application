ea7dfb708f77fa93d924209ed630cb133960ff1a
Austin Shalit <austinshalit@gmail.com>
2019-04-05 23:57:21 -0400

    Use strategy pattern for pathfinding (#210)
    * Use strategy pattern for pathfinding
    
    * Add getters and setters per Wong

---
+++ src/main/java/edu/wpi/cs3733/d19/teamO/entity/pathfinding/BreadthFirstSearchAlgorithm.java
@@ -0,0 +1,35 @@
+package edu.wpi.cs3733.d19.teamO.entity.pathfinding;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+import com.google.common.graph.Graph;
+
+class BreadthFirstSearchAlgorithm<T> extends UninformedGraphSearchAlgorithm<T> {
+  @Override
+  public List<T> getPath(final Graph<T> graph, final T start, final T goal) {
+    final Queue<T> frontier = new LinkedList<>();
+    final Map<T, T> cameFrom = new HashMap<>();
+    frontier.add(start);
+    cameFrom.put(start, null);
+
+    while (!frontier.isEmpty()) {
+      T current = frontier.poll();
+
+      if (current.equals(goal)) {
+        break;
+      }
+
+      for (T next : graph.successors(current)) {
+        if (!cameFrom.containsKey(next)) {
+          frontier.add(next);
+          cameFrom.put(next, current);
+        }
+      }
+    }
+    return buildPath(cameFrom, goal);
+  }
+}
---
+++ src/main/java/edu/wpi/cs3733/d19/teamO/entity/pathfinding/DepthFirstSearchAlgorithm.java
@@ -0,0 +1,34 @@
+package edu.wpi.cs3733.d19.teamO.entity.pathfinding;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+import com.google.common.graph.Graph;
+
+class DepthFirstSearchAlgorithm<T> extends UninformedGraphSearchAlgorithm<T> {
+  @Override
+  public List<T> getPath(final Graph<T> graph, final T start, final T goal) {
+    final Stack<T> frontier = new Stack<>();
+    final Map<T, T> cameFrom = new HashMap<>();
+    frontier.push(start);
+    cameFrom.put(start, null);
+
+    while (!frontier.isEmpty()) {
+      T current = frontier.pop();
+
+      if (current.equals(goal)) {
+        break;
+      }
+
+      for (T next : graph.successors(current)) {
+        if (!cameFrom.containsKey(next)) {
+          frontier.push(next);
+          cameFrom.put(next, current);
+        }
+      }
+    }
+    return buildPath(cameFrom, goal);
+  }
+}
