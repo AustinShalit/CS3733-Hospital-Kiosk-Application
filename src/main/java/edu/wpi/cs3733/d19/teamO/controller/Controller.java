4ef2fa58507c0f2c77f4a77752e5202865ea33e2
Ken Morton Jr <kmorton15@gmail.com>
2019-04-07 14:01:30 -0400

    fixed combobox text overlay glitch

--- src/main/java/edu/wpi/cs3733/d19/teamO/controller/Controller.java
+++ src/main/java/edu/wpi/cs3733/d19/teamO/controller/Controller.java
@@ -9,6 +9,8 @@ import com.jfoenix.controls.JFXComboBox;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -120,7 +122,14 @@ public class Controller {
           }
         };
     comboBox.setCellFactory(cellFactory);
-    comboBox.setButtonCell(cellFactory.call(null));
+
+    // wait for selection
+    comboBox.valueProperty().addListener(new ChangeListener<Node>() {
+      @Override
+      public void changed(ObservableValue<? extends Node> observable, Node oldValue, Node newValue) {
+        comboBox.setButtonCell(cellFactory.call(null));
+      }
+    });
   }
 
   /**
