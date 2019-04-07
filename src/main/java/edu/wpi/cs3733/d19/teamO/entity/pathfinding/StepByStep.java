41d6e817840a42856d9222dca8d670516cb4bbb1
Sam Johnson <smjohnson635@gmail.com>
2019-04-07 12:42:40 -0400

    tested. might need to be refactored

--- src/main/java/edu/wpi/cs3733/d19/teamO/entity/pathfinding/StepByStep.java
+++ src/main/java/edu/wpi/cs3733/d19/teamO/entity/pathfinding/StepByStep.java
@@ -1,6 +1,7 @@
 package edu.wpi.cs3733.d19.teamO.entity.pathfinding;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 import edu.wpi.cs3733.d19.teamO.entity.Node;
@@ -12,31 +13,40 @@ public class StepByStep {
 
   public String getDirection(Coordinate start, Coordinate end) {
     int xDif = end.getxCoord() - start.getxCoord();
-    int ydif = start.getyCoord() - end.getyCoord();
+    int yDif = start.getyCoord() - end.getyCoord();
 
-    double theta = Math.atan2(xDif, ydif) + 1.5708;
+    double theta = Math.atan2(yDif, xDif)*180/Math.PI;
 
 
-    if (theta < .78 || theta > 5.49779) {
+
+    if (theta > 22.5 && theta <= 67.5) {
+      return "North East";
+    } else if (theta > 67.5 && theta <= 112.5) {
       return "North";
-    } else if (theta >= .78 && theta <= 2.35) {
+    } else if (theta > 112.5 && theta <= 157.5) {
+      return "North West";
+    } else if (theta > 157.5 || theta <= -157.5) {
       return "West";
-    } else if (theta > 2.35 && theta <= 3.92699) {
+    } else if (theta < -112.5 && theta > -157.5) {
+      return "South West";
+    } else if (theta > -112.5 && theta <= -67.5) {
       return "South";
-    } else if (theta > 3.92699 && theta <= 5.49779) {
+    } else if (theta > -67.5 && theta <= -22.5) {
+      return "South East";
+    } else if (theta > -22.5 || theta <= 22.5) {
       return "East";
     }
 
     return "";
   }
 
-  public ArrayList<String> getStepByStep(Stack<Node> nodes) {
+  public ArrayList<String> getStepByStep(List<Node> nodes) {
     ArrayList<String> instructions = new ArrayList<>();
 
     ArrayList<Coordinate> coordinates= new ArrayList<>();
 
     for (Node node: nodes) {
-      Coordinate newCoord = new Coordinate(nodes.peek().getXcoord(),nodes.peek().getYcoord());
+      Coordinate newCoord = new Coordinate(node.getXcoord(),node.getYcoord());
       coordinates.add(newCoord);
     }
 
@@ -51,13 +61,30 @@ public class StepByStep {
       String current = cardinalDirections.get(i);
       String next = cardinalDirections.get(i+1);
 
+      /**
+       * Right
+       *  North, East;
+       * Left
+       *  Current:
+       *  Next:
+       */
       if (current.equals("North")) {
         if (next.equals("East")) {
           instructions.add("Right");
+        } else if (next.equals("North East")) {
+          instructions.add("Slight Right");
+        } else if (next.equals("North West")) {
+          instructions.add("Slight Left");
+        } else if (next.equals("South West")) {
+          instructions.add("Hard Left");
+        } else if (next.equals("South East")) {
+          instructions.add("Hard Right");
         } else if (next.equals("West")) {
           instructions.add("Left");
         } else if (next.equals("North")) {
           instructions.add("Forward");
+        } else if (next.equals("South")) {
+          instructions.add("None");
         }
       } else if (current.equals("East")) {
         if (next.equals("South")) {
@@ -66,6 +93,16 @@ public class StepByStep {
           instructions.add("Left");
         } else if (next.equals("East")) {
           instructions.add("Forward");
+        } else if (next.equals("West")) {
+          instructions.add("None");
+        } else if (next.equals("North East")) {
+          instructions.add("Slight Left");
+        } else if (next.equals("South East")) {
+          instructions.add("Slight Right");
+        } else if (next.equals("North West")) {
+          instructions.add("Hard Left");
+        } else if (next.equals("South West")) {
+          instructions.add("Hard Right");
         }
       } else if (current.equals("South")) {
         if (next.equals("West")) {
@@ -74,6 +111,16 @@ public class StepByStep {
           instructions.add("Left");
         } else if (next.equals("South")) {
           instructions.add("Forward");
+        } else if (next.equals("North")) {
+          instructions.add("None");
+        } else if (next.equals("South East")) {
+          instructions.add("Slight Left");
+        } else if (next.equals("South West")) {
+          instructions.add("Slight Right");
+        } else if (next.equals("North West")) {
+          instructions.add("Hard Right");
+        } else if (next.equals("North East")) {
+          instructions.add("Hard Left");
         }
       } else if (current.equals("West")) {
         if (next.equals("North")) {
@@ -81,6 +128,88 @@ public class StepByStep {
         } else if (next.equals("South")) {
           instructions.add("Left");
         } else if (next.equals("West")) {
+          instructions.add("Forward");
+        } else if (next.equals("East")) {
+          instructions.add("None");
+        } else if (next.equals("North West")) {
+          instructions.add("Slight Right");
+        } else if (next.equals("South West")) {
+          instructions.add("Slight Left");
+        } else if (next.equals("North East")) {
+          instructions.add("Hard Right");
+        } else if (next.equals("South East")) {
+          instructions.add("Hard Left");
+        }
+      } else if (current.equals("North West")) {
+        if (next.equals("West")) {
+          instructions.add("Slight Left");
+        } else if (next.equals("North")) {
+          instructions.add("Slight Right");
+        } else if (next.equals("South West")) {
+          instructions.add("Left");
+        } else if (next.equals("North East")) {
+          instructions.add("Right");
+        } else if (next.equals("South")) {
+          instructions.add("Hard Left");
+        } else if (next.equals("East")) {
+          instructions.add("Hard Right");
+        } else if (next.equals("North West")) {
+          instructions.add("Forward");
+        } else if (next.equals("South East")) {
+          instructions.add("None");
+        }
+      } else if (current.equals("North East")) {
+        if (next.equals("North")) {
+          instructions.add("Slight Left");
+        } else if (next.equals("East")) {
+          instructions.add("Slight Right");
+        } else if (next.equals("North West")) {
+          instructions.add("Left");
+        } else if (next.equals("South East")) {
+          instructions.add("Right");
+        } else if (next.equals("South")) {
+          instructions.add("Hard Right");
+        } else if (next.equals("West")) {
+          instructions.add("Hard Left");
+        } else if (next.equals("South West")) {
+          instructions.add("None");
+        } else if (next.equals("North East")) {
+          instructions.add("Forward");
+        }
+      } else if (current.equals("South East")) {
+        if (next.equals("East")) {
+          instructions.add("Slight Left");
+        } else if (next.equals("South")) {
+          instructions.add("Slight Right");
+        } else if (next.equals("North East")) {
+          instructions.add("Right");
+        } else if (next.equals("South West")) {
+          instructions.add("Right");
+        } else if (next.equals("North")) {
+          instructions.add("Hard Left");
+        } else if (next.equals("West")) {
+          instructions.add("Hard Right");
+        } else if (next.equals("North West")) {
+          instructions.add("None");
+        } else if (next.equals("South East")) {
+          instructions.add("Forward");
+        }
+      } else if (current.equals("South West")) {
+        if (next.equals("South")) {
+          instructions.add("Slight Left");
+        } else if (next.equals("West")) {
+          instructions.add("Slight Right");
+        } else if (next.equals("North West")) {
+          instructions.add("Right");
+        } else if (next.equals("South East")) {
+          instructions.add("Left");
+        } else if (next.equals("North")) {
+          instructions.add("Hard Right");
+        } else if (next.equals("East")) {
+          instructions.add("Hard Left");
+        } else if (next.equals("North East")) {
+          instructions.add("None");
+        } else if (next.equals("South West")) {
           instructions.add("Forward");
         }
       }
---
+++ src/test/java/edu/wpi/cs3733/d19/teamO/entity/pathfinding/StepByStepTest.java
@@ -0,0 +1,77 @@
+package edu.wpi.cs3733.d19.teamO.entity.pathfinding;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import edu.wpi.cs3733.d19.teamO.entity.Edge;
+import edu.wpi.cs3733.d19.teamO.entity.Node;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StepByStepTest {
+
+
+  /*
+  @Test
+  void getDirection() {
+  }
+
+  */
+  @Test
+  void getStepByStep() {
+
+    final Node NODE_A = new Node("A", 0, 0, "0", "B", Node.NodeType.HALL,
+        "AL", "AS");
+    final Node NODE_B = new Node("B", 1, 0, "0", "B", Node.NodeType.HALL,
+        "BL", "BS");
+    final Node NODE_C = new Node("C", 2, 1, "0", "B", Node.NodeType.HALL,
+        "CL", "CS");
+    final Node NODE_D = new Node("D", 2, 2, "0", "B", Node.NodeType.HALL,
+        "CL", "CS");
+    final Node NODE_E = new Node("E", 1, 1, "0", "B", Node.NodeType.HALL,
+        "CL", "CS");
+    final Node NODE_F = new Node("F", 1, 3, "0", "B", Node.NodeType.HALL,
+        "CL", "CS");
+    final Node NODE_G = new Node("G", 2, 3, "0", "B", Node.NodeType.HALL,
+        "CL", "CS");
+    final Node NODE_H = new Node("H", 2, 4, "0", "B", Node.NodeType.HALL,
+        "CL", "CS");
+    final Node NODE_I = new Node("I", 3, 5, "0", "B", Node.NodeType.HALL,
+        "CL", "CS");
+
+
+
+
+    List<Node> testNodes = new ArrayList<Node>();
+    testNodes.add(NODE_A);
+    testNodes.add(NODE_B);
+    testNodes.add(NODE_C);
+    testNodes.add(NODE_D);
+    testNodes.add(NODE_E);
+    testNodes.add(NODE_F);
+    testNodes.add(NODE_G);
+    testNodes.add(NODE_H);
+    testNodes.add(NODE_I);
+
+
+
+
+    ArrayList<String> answer = new ArrayList<>();
+    answer.add("Slight Right");
+    answer.add("Slight Right");
+    answer.add("Hard Right");
+    answer.add("Hard Left");
+    answer.add("Left");
+    answer.add("Right");
+    answer.add("Slight Left");
+
+
+    StepByStep stepByStep = new StepByStep();
+    ArrayList<String> test = stepByStep.getStepByStep(testNodes);
+    assertEquals(answer, test);
+  }
+}
