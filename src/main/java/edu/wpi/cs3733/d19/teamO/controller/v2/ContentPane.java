094a37f0c3da5b66acfc83142af2fa879c47f5a5
Austin Shalit <austinshalit@gmail.com>
2019-04-06 23:36:19 -0400

    Setup EventBus for chaning views

---
+++ src/main/java/edu/wpi/cs3733/d19/teamO/controller/v2/ContentPane.java
@@ -0,0 +1,7 @@
+package edu.wpi.cs3733.d19.teamO.controller.v2;
+
+import com.google.common.eventbus.EventBus;
+
+public interface ContentPane {
+  void setEventBus(EventBus eventBus);
+}
--- src/main/java/edu/wpi/cs3733/d19/teamO/controller/v2/MainController.java
+++ src/main/java/edu/wpi/cs3733/d19/teamO/controller/v2/MainController.java
@@ -2,9 +2,13 @@ package edu.wpi.cs3733.d19.teamO.controller.v2;
 
 import java.io.IOException;
 
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import com.jfoenix.controls.JFXPopup;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Node;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 
@@ -14,11 +18,42 @@ public class MainController {
   private StackPane root;
 
   @FXML
-  private BorderPane content;
+  private BorderPane contentPane;
+
+  @FXML
+  private StackPane optionsBurger;
+
+  private final EventBus contentEventBus = new EventBus("Main Content EventBus");
+
+  private JFXPopup optionsPopup;
+
+  public MainController() {
+    contentEventBus.register(this);
+  }
 
   @FXML
   void initialize() throws IOException {
-    Parent view = FXMLLoader.load(MainController.class.getResource("Home.fxml"));
-    content.setCenter(view);
+    Node view = FXMLLoader.load(MainController.class.getResource("Home.fxml"));
+    contentPane.setCenter(view);
+
+    FXMLLoader loader = new FXMLLoader(MainController.class.getResource("OptionsPopup.fxml"));
+    optionsPopup = new JFXPopup(loader.load());
+
+    optionsBurger.setOnMouseClicked(e ->
+        optionsPopup.show(optionsBurger,
+            JFXPopup.PopupVPosition.TOP,
+            JFXPopup.PopupHPosition.RIGHT,
+            -12,
+            15));
+  }
+
+  @Subscribe
+  public void onContentChangeRequest(String r) throws IOException {
+    FXMLLoader loader = new FXMLLoader();
+    loader.setLocation(MainController.class.getResource(r));
+    ContentPane c = loader.getController();
+    c.setEventBus(contentEventBus);
+    Node view = loader.load();
+    contentPane.setCenter(view);
   }
 }
