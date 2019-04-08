41fbd0b35f33b5f8fc43d48505f4f201bca5ba73
Jack Yang <yyang17@wpi.edu>
2019-04-07 23:16:20 -0400

    Fix Traivs

--- src/main/java/edu/wpi/cs3733/d19/teamO/entity/database/Database.java
+++ src/main/java/edu/wpi/cs3733/d19/teamO/entity/database/Database.java
@@ -14,7 +14,8 @@ import edu.wpi.cs3733.d19.teamO.entity.SanitationRequest;
 import edu.wpi.cs3733.d19.teamO.entity.SchedulingRequest;
 import edu.wpi.cs3733.d19.teamO.entity.SecurityRequest;
 
-@SuppressWarnings("PMD.TooManyMethods")
+
+@SuppressWarnings({"PMD.GodClass", "PMD.TooManyMethods", "PMD.ExcessivePublicCount"})
 public class Database {
 
   private final NodeDao nodeDao;
