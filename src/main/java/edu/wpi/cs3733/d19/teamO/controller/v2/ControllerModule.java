82b2cd2289168a643cf04e62e6e98e02900864df
jcharante <git@jcharante.com>
2019-04-08 23:28:25 -0400

    supress warning

--- src/main/java/edu/wpi/cs3733/d19/teamO/controller/v2/ControllerModule.java
+++ src/main/java/edu/wpi/cs3733/d19/teamO/controller/v2/ControllerModule.java
@@ -25,6 +25,7 @@ import edu.wpi.cs3733.d19.teamO.controller.v2.request.InterpreterViewController;
 import edu.wpi.cs3733.d19.teamO.controller.v2.request.PatientInfoController;
 import edu.wpi.cs3733.d19.teamO.controller.v2.request.PatientInfoViewController;
 
+@SuppressWarnings("PMD.CouplingBetweenObjects")
 public class ControllerModule extends AbstractModule {
   @Override
   protected void configure() {
