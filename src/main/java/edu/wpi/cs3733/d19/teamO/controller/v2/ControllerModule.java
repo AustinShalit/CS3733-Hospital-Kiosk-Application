4289dab5e3e7310a922622c646b29be5e39f7316
Ken Morton Jr <kmorton15@gmail.com>
2019-04-09 00:45:19 -0400

    fixed travis error

--- src/main/java/edu/wpi/cs3733/d19/teamO/controller/v2/ControllerModule.java
+++ src/main/java/edu/wpi/cs3733/d19/teamO/controller/v2/ControllerModule.java
@@ -25,6 +25,7 @@ import edu.wpi.cs3733.d19.teamO.controller.v2.request.InternalTransportationView
 import edu.wpi.cs3733.d19.teamO.controller.v2.request.InterpreterController;
 import edu.wpi.cs3733.d19.teamO.controller.v2.request.InterpreterViewController;
 
+@SuppressWarnings("PMD.CouplingBetweenObjects")
 public class ControllerModule extends AbstractModule {
   @Override
   protected void configure() {
