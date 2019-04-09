49ff4741417229b9971d80e985858bab14cdcfe9
Alexandria Nogueras <aenogueras@wpi.edu>
2019-04-08 21:41:45 -0400

    Merge branch 'nice_colors' of C:\Users\Alexandria Nogueras\IdeaProjects\CS3733-D19-Team-O-Project with conflicts.

---
+++ src/main/java/edu/wpi/cs3733/d19/teamO/controller/v2/AdminController.java
@@ -0,0 +1,49 @@
+package edu.wpi.cs3733.d19.teamO.controller.v2;
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.layout.VBox;
+
+@FxmlController(url = "AdminWindow.fxml")
+public class AdminController implements Controller {
+
+  @FXML
+  private VBox root;
+  @FXML
+  private JFXButton mapeditButton;
+  @FXML
+  private JFXButton employeeButton;
+  @FXML
+  private JFXButton editdatabaseButton;
+
+  @FXML
+  void initialize() {
+    System.out.println("weiogeigbiawoegbaoweigbaweiogbaewogbaewogiwe");
+  }
+
+  @FXML
+  void mapeditOnAction() {
+
+  }
+
+  @FXML
+  void employeeOnAction() {
+
+  }
+
+  @FXML
+  void editdatabaseOnAction() {
+    switchScenes("EditedNodesForExport.fxml", editdatabaseButton.getScene().getWindow());
+  }
+
+  @Override
+  public Parent getRoot() {
+    return root;
+  }
+
+  public interface Factory {
+    AdminController create();
+  }
+}
