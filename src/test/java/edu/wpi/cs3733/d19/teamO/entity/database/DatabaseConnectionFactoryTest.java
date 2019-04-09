7a13c4999a58508629f07470f336acae0478e29e
jcharante <git@jcharante.com>
2019-04-09 11:24:45 -0400

    refactor test to close

--- src/test/java/edu/wpi/cs3733/d19/teamO/entity/database/DatabaseConnectionFactoryTest.java
+++ src/test/java/edu/wpi/cs3733/d19/teamO/entity/database/DatabaseConnectionFactoryTest.java
@@ -1,5 +1,6 @@
 package edu.wpi.cs3733.d19.teamO.entity.database;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
 import org.junit.jupiter.api.Test;
@@ -10,8 +11,10 @@ class DatabaseConnectionFactoryTest {
 
   @Test
   void getConnectionTest() throws SQLException {
-    assertNotNull(
-        new DatabaseConnectionFactoryEmbedded(DatabaseConnectionFactoryEmbedded.MEMORY_PROTOCOL,
-            "Test").getConnection());
+    try (Connection con = new DatabaseConnectionFactoryEmbedded(
+        DatabaseConnectionFactoryEmbedded.MEMORY_PROTOCOL,
+        "Test").getConnection()) {
+      assertNotNull(con);
+    }
   }
 }
