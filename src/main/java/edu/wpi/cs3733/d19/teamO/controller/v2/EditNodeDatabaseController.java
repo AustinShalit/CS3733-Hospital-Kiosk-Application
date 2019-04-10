0a74800725e78387f25be1721a079c2ad1c23bbc
Ken Morton Jr <kmorton15@gmail.com>
2019-04-10 05:53:25 -0400

    merge master into ui

--- src/main/java/edu/wpi/cs3733/d19/teamO/controller/v2/EditNodeDatabaseController.java
+++ src/main/java/edu/wpi/cs3733/d19/teamO/controller/v2/EditNodeDatabaseController.java
@@ -1,9 +1,10 @@
 package edu.wpi.cs3733.d19.teamO.controller.v2;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 import com.google.inject.Inject;
 import com.jfoenix.controls.JFXButton;
@@ -19,7 +20,9 @@ import edu.wpi.cs3733.d19.teamO.entity.csv.EdgeCsvReaderWriter;
 import edu.wpi.cs3733.d19.teamO.entity.csv.NodeCsvReaderWriter;
 import edu.wpi.cs3733.d19.teamO.entity.database.Database;
 
+
 @FxmlController(url = "EditNodeDatabase.fxml")
+@SuppressWarnings("PMD.AvoidFileStream")
 public class EditNodeDatabaseController implements Controller {
 
   @FXML
@@ -36,19 +39,25 @@ public class EditNodeDatabaseController implements Controller {
 
   }
 
+  /**
+   * Export to a csv file.
+   */
+
   @FXML
-  public void onExportButtonAction() throws IOException, CsvDataTypeMismatchException, CsvRequiredFieldEmptyException {
+  public void onExportButtonAction() throws IOException, CsvDataTypeMismatchException,
+      CsvRequiredFieldEmptyException {
     final DirectoryChooser directoryChooser = new DirectoryChooser();
     final File selectedDirectory = directoryChooser.showDialog(getRoot().getScene().getWindow());
     final File nodeFile = new File(selectedDirectory, "nodes.csv");
     final File edgesFile = new File(selectedDirectory, "edges.csv");
 
     if (selectedDirectory != null) {
-      FileWriter fw = new FileWriter(nodeFile);
+      Writer fw = new OutputStreamWriter(new FileOutputStream(nodeFile), "UTF-8");
+
       NodeCsvReaderWriter nodeCsvReaderWriter = new NodeCsvReaderWriter();
       nodeCsvReaderWriter.writeNodes(fw, database.getAllNodes());
 
-      fw = new FileWriter(edgesFile);
+      fw = new OutputStreamWriter(new FileOutputStream(edgesFile), "UTF-8");
       EdgeCsvReaderWriter edgeCsvReaderWriter = new EdgeCsvReaderWriter(database);
       edgeCsvReaderWriter.writeNodes(fw, database.getAllEdges());
 
