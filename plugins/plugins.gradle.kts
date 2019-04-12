eae8883e02d826bce9d350bd0886f67ca1dd8a0d
Austin Shalit <austinshalit@gmail.com>
2019-04-11 20:53:13 -0400

    Add Request plugin module

---
+++ plugins/plugins.gradle.kts
@@ -0,0 +1,17 @@
+subprojects {
+    apply(plugin = "java-library")
+
+    afterEvaluate {
+        val sourceJar = task<Jar>("sourceJar") {
+            description = "Creates a JAR that contains the source code."
+            from(project.the<SourceSetContainer>()["main"].allSource)
+            archiveClassifier.set("sources")
+        }
+        val javadocJar = task<Jar>("javadocJar") {
+            dependsOn("javadoc")
+            description = "Creates a JAR that contains the javadocs."
+            from(tasks.named("javadoc"))
+            archiveClassifier.set("javadoc")
+        }
+    }
+}
