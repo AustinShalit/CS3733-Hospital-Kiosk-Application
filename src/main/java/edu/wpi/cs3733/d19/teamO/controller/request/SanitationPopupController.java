07a1d2e9b7a0520476dfe73d9dd5d17caa14d3a7
ameliaNishimura <anishimura@wpi.edu>
2019-04-14 13:14:17 -0400

    submit request is disabled if not all fields are filled out (sanitation only for now)

--- src/main/java/edu/wpi/cs3733/d19/teamO/controller/request/SanitationPopupController.java
+++ src/main/java/edu/wpi/cs3733/d19/teamO/controller/request/SanitationPopupController.java
@@ -12,6 +12,7 @@ import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
@@ -49,6 +50,13 @@ public class SanitationPopupController implements Controller {
   void initialize() {
     DialogHelper.populateComboBox(db, locationbox);
     categorybox.getItems().setAll(SanitationRequest.SanitationRequestType.values());
+
+    submitbtn.disableProperty().bind(
+        Bindings.isEmpty(nametxt.textProperty())
+        .or(Bindings.isNull(locationbox.valueProperty()))
+        .or(Bindings.isNull(categorybox.valueProperty()))
+        .or(Bindings.isEmpty(descriptiontxt.textProperty()))
+    );
   }
 
   @FXML
