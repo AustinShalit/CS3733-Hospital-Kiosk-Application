6da32cb989bbc7d97f02452a1533fb22307ab908
Austin Shalit <austinshalit@gmail.com>
2019-04-15 16:27:48 -0400

    Add Request plugin module (#409)
    * Add Request plugin module
    
    * Fix spotless & apply

---
+++ plugins/request/src/main/java/edu/wpi/cs3733/d19/teamO/request/Request.java
@@ -0,0 +1,9 @@
+package edu.wpi.cs3733.d19.teamO.request;
+
+public class Request implements IRequest {
+  @Override
+  public void run(int xcoord, int ycoord, int windowWidth, int windowLength, String cssPath,
+                  String destNodeID, String originNodeID) {
+
+  }
+}
