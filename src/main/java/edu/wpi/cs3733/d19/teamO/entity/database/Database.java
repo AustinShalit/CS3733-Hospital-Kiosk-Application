3e5a87f8d4b88f46cdd67ba0eed974aa6a1f73a5
Lynn <jsong@wpi.edu>
2019-04-15 17:46:41 -0400

    Filter combo box to contain only the wanted nodes

--- src/main/java/edu/wpi/cs3733/d19/teamO/entity/database/Database.java
+++ src/main/java/edu/wpi/cs3733/d19/teamO/entity/database/Database.java
@@ -122,11 +122,6 @@ public class Database implements AutoCloseable {
     return nodeDao.getFreeNodeId();
   }
 
-  @Deprecated
-  public List<Node> getAllByLongName() {
-    return nodeDao.getAllByLongname();
-  }
-
   public List<Node> getAllNodesByLongName() {
     return nodeDao.getAllByLongname();
   }
