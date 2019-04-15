fd38c6eec96be2ee6bbc6b070364cf82bff61450
ameliaNishimura <48456549+ameliaNishimura@users.noreply.github.com>
2019-04-14 20:42:48 -0400

    308 popups (#448)
    * Revert "Did admin window and added icon to option toolbar (#257)"
    
    This reverts commit 3325f0d28f35e1eef58ac13564ce18a7ccfdbf24.
    
    * cool new popup window for adding A/V requests
    
    * complete request and assign employee buttons are now disabled if no request is selected
    
    * spotless
    
    * converted external transportation request page
    
    * sanitation reqest popup
    
    * submit request is disabled if not all fields are filled out (sanitation only for now)
    
    * added submit button disable to other revamped request pages
    
    * prescription request revamped
    
    * interpreter request page revamped
    
    * fixed the issue of not all interpreter fields appearing
    
    * internal request page revamped
    
    * patient info request page revamped
    
    * gift request page revamped
    
    * different method of positioning popup for sanitation
    
    * IT support page revamped
    
    * religious request page revamped
    
    * support animal request page revamp
    
    * florist request page revamped
    
    * removed "check service request" page and option in dropdown menu
    
    * description is now optional field

--- src/main/java/edu/wpi/cs3733/d19/teamO/controller/request/PatientInfoViewController.java
+++ src/main/java/edu/wpi/cs3733/d19/teamO/controller/request/PatientInfoViewController.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import com.google.common.eventbus.EventBus;
 import com.google.inject.Inject;
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPopup;
 
 import animatefx.animation.Shake;
 import javafx.fxml.FXML;
@@ -14,12 +15,13 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.effect.ColorAdjust;
 import javafx.scene.layout.BorderPane;
 
-import edu.wpi.cs3733.d19.teamO.controller.CheckRequestsController;
 import edu.wpi.cs3733.d19.teamO.controller.Controller;
 import edu.wpi.cs3733.d19.teamO.controller.DialogHelper;
 import edu.wpi.cs3733.d19.teamO.controller.FxmlController;
+import edu.wpi.cs3733.d19.teamO.controller.RequestController;
 import edu.wpi.cs3733.d19.teamO.controller.event.ChangeMainViewEvent;
 import edu.wpi.cs3733.d19.teamO.entity.PatientInfoRequest;
 import edu.wpi.cs3733.d19.teamO.entity.database.Database;
@@ -60,7 +62,11 @@ public class PatientInfoViewController implements Controller {
   @Inject
   private Database db;
   @Inject
-  private CheckRequestsController.Factory checkRequestsControllerFactory;
+  private RequestController.Factory checkRequestsControllerFactory;
+  @Inject
+  private PatientInfoPopupController.Factory patientInfoPopupFactory;
+
+  private JFXPopup addPopup;
 
   @FXML
   void initialize() {
@@ -84,11 +90,53 @@ public class PatientInfoViewController implements Controller {
     for (TableColumn column : requestsTableView.getColumns()) {
       column.setPrefWidth(1000); // must be a value larger than the starting window size
     }
+
+    // Initialize the Add Request Popup
+    addPopup = new JFXPopup(patientInfoPopupFactory.create().root);
+    addPopup.setOnAutoHide(
+        event -> {
+          ColorAdjust reset = new ColorAdjust();
+          reset.setBrightness(0);
+          root.setEffect(reset);
+          requestsTableView.getItems().clear();
+          requestsTableView.getItems().setAll(db.getAllPatientInfoRequests());
+        }
+    );
+
+    // assignButton.setDisable(true);
+    deleteEntryButton.setDisable(true);
+
+    // Disable complete request and assign employee button if no request is selected
+    requestsTableView.getSelectionModel().selectedItemProperty().addListener(
+        (observable, oldValue, newValue) -> {
+          if (newValue == null) {
+            // assignButton.setDisable(true);
+            deleteEntryButton.setDisable(true);
+          } else {
+            // assignButton.setDisable(false);
+            deleteEntryButton.setDisable(false);
+          }
+        }
+    );
   }
 
   @FXML
   void goBackButtonAction() {
     eventBus.post(new ChangeMainViewEvent(checkRequestsControllerFactory.create()));
+  }
+
+  @FXML
+  void onAddButtonAction() {
+    ColorAdjust colorAdjust = new ColorAdjust();
+    colorAdjust.setBrightness(-0.2);
+    root.setEffect(colorAdjust);
+    addPopup.show(root);
+    addPopup.setX(
+        (root.getScene().getWindow().getWidth() - addPopup.getWidth()) / 2
+    );
+    addPopup.setY(
+        (root.getScene().getWindow().getHeight() - addPopup.getHeight()) / 2
+    );
   }
 
   @FXML
