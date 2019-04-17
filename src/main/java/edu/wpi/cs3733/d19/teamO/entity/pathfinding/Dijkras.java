4d68634b8300fe2fd871e8599be6f8cc85839b24
Sam Johnson <smjohnson635@gmail.com>
2019-04-17 03:23:24 -0400

    added dijkras

---
+++ src/main/java/edu/wpi/cs3733/d19/teamO/entity/pathfinding/Dijkras.java
@@ -0,0 +1,79 @@
+package edu.wpi.cs3733.d19.teamO;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Stack;
+
+import edu.wpi.cs3733.d19.teamO.entity.Node;
+import edu.wpi.cs3733.d19.teamO.entity.database.Database;
+
+public class DijkrasAlgorithm extends SearchAlgorithm {
+
+  public DijkrasAlgorithm(Database database) {
+    super(database);
+  }
+
+  private static final class Pair implements Comparable<Pair> {
+    Node pairNode;
+    double pairCost;
+
+    Pair(Node pairNode, Double pairCost) {
+      this.pairNode = pairNode;
+      this.pairCost = pairCost;
+    }
+
+    public void setPairNode(Node pairNode) {
+      this.pairNode = pairNode;
+    }
+
+    public void setPairCost(double pairCost) {
+      this.pairCost = pairCost;
+    }
+
+    @Override
+    public int compareTo(Pair pair) {
+      return Double.compare(pairCost, pair.pairCost);
+    }
+
+
+  }
+
+  Stack<Node> getPath(Node start, Node goal) {
+    PriorityQueue<Pair> frontier = new PriorityQueue<>();
+
+    Map<Node, Node> cameFrom = new HashMap<>();
+    cameFrom.put(start, null);
+    Map<Node, Double> costSoFar = new HashMap<>();
+    Pair newPair = new Pair(start , 0.0);
+    frontier.add(newPair);
+    costSoFar.put(start, 0.0);
+
+    Pair nextPair = new Pair(start, 0.0);
+
+    while (!frontier.isEmpty()) {
+      Node current = frontier.poll().pairNode;
+
+      if (current.equals(goal)) {
+        break;
+      }
+
+
+      for (Node next : neighbors(current)) {
+        Double newCost = costSoFar.get(current) + current.getCost(next);
+        if (!cameFrom.containsKey(next) || newCost < costSoFar.get(next)) {
+          double priority = newCost;
+          nextPair.setPairNode(next);
+          nextPair.setPairCost(priority);
+          frontier.add(nextPair);
+          costSoFar.put(next, newCost);
+          cameFrom.put(next, current);
+        }
+      }
+    }
+
+    return makePath(cameFrom, goal);
+  }
+
+
+}
